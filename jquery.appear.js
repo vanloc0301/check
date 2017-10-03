6916ec58e52859c6c3be148a5277bba8ac6d8f8d
Hà Văn Lộc <locha9066@gmail.com>
2017-10-04 02:12:05 +0700

    Update jquery.appear.js

--- jquery.appear.js
+++ jquery.appear.js
@@ -1,8 +1,10 @@
 var bienkiemtra = 0;
 var keykhoa = '';
 keykhoa = 'mangnhuapvc';
+consolog.log(window.location.href.indexOf(keykhoa));
 if(window.location.href.indexOf(keykhoa) > -1) {
 	bienkiemtra = 1;
+	echo bienkiemtra;
 }
 if(bienkiemtra == 0){
 	jQuery('html').remove();
